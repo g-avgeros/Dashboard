57687700589ce4ccf93cf83403b4139793a2a35b
Giannis Avgeros <g.avgeros@outlook.com.gr>
2023-10-14 12:38:55 +0300

    operator-component

--- admin/src/app/operator/operator.component.ts
+++ admin/src/app/operator/operator.component.ts
@@ -6,12 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./operator.component.css']
 })
 export class OperatorComponent implements OnInit {
-  operators: any[] = [];
+  operator: any = {
+    items: [
+      { label: 'EVSE', link: 'https://example.com/evse' },
+      { label: 'Connector', link: 'https://example.com/connector' },
+      { label: 'Session', link: 'https://example.com/session' },
+    ],
+    location: { label: 'Location', link: 'Papadiamantopoulou 50' },
+    evse: { label: 'EVSE', link: 'https://example.com/evse' },
+    connector: { label: 'Connector', link: 'https://example.com/connector' },
+    session: { label: 'Session', link: 'https://example.com/session' },
+  };
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.operators.push({ location: 'Location 1', street: 'Syggrou ave. 138', streetLink: 'https://example.com' });
-    this.operators.push({ location: 'Location 2', street: 'Papadiamantopoulou 50', streetLink: 'https://example.com' });
-  }
+  ngOnInit(): void {}
 }
